3a157ad66f46bac9785d42618efbcef34e11c750
chenzhichao <80139283@qq.com>
2025-09-21 20:54:22 +0800

    add

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let mut prost_build = prost_build::Config::new();
     // prost_build.protoc_executable("protoc-27.1");
-    // tonic_prost_build::compile_protos("proto/helloworld.proto")?;
+    tonic_prost_build::compile_protos("proto/*.proto")?;
     // prost_build::compile_protos(&["proto/helloworld.proto"], &["src/"])?;
 
     Ok(())
